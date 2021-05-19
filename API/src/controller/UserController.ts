a936ae3ce8fd35a027c90bcf5ac5d31c23301a5e
Luiz Cavalcanti <luizdantas.cavalcanti@gmail.com>
2021-05-18 23:32:48 -0300

    finalize user CRUD (without validations)

--- API/src/controller/UserController.ts
+++ API/src/controller/UserController.ts
@@ -7,36 +7,55 @@ export const getUsers = async (request: Request, response: Response) => {
   return response.json(users);
 };
 
+const hash = (password:string) => {
+  // TODO: ACTUALLY HASH THIS. VERY IMPORTANT.
+  return password;
+}
+
 export const createUser = async (request: Request, response: Response) => {
   let {type} = request.body;
   type = Number(type);
+  const {primary_email, secondary_email, password,
+         legal_name, legal_id,
+         address, area, city, state, CEP, mobile_phone} = request.body;
+  // TODO validar email com package isemail
+  // TODO validar email secundario com package isemail
+  // TODO validar password de acordo com as regras que estabelecermos
+  // TODO validar nome????
+  // TODO validar estado
+  // TODO validar CEP
+  // TODO validar celular
+  const password_hash = hash(password);
+  let user: User;
   switch (type) {
     case user_types.candidate:
-      const {primary_email, secondary_email, password,
-             legal_name, registration_number, legal_id,
-             address, area, city, state, CEP, mobile_phone} = request.body;
-      // TODO validar email com package isemail
-      // TODO validar email secundario com package isemail
-      // TODO validar password de acordo com as regras que estabelecermos
-      // TODO validar nome????
+      const {registration_number} = request.body;
       // TODO validar numero de matricula
       // TODO validar CPF
-      // TODO validar estado
-      // TODO validar CEP
-      // TODO validar celular
-      const user = await getRepository(User).save({type,primary_email, secondary_email, password,
+       // TODO: USAR BCRYPT AQUI
+      user = await getRepository(User).save({type, primary_email, secondary_email, ,
              legal_name, registration_number, legal_id,
              address, area, city, state, CEP, mobile_phone});
       return response.json({message: "user created in database", user});
       break;
     case user_types.company:
-      const {} = request.body;
+      const {alternative_name, employee_name } = request.body;
+      // TODO validar nome fantasia ?????
+      // TODO validar nome do funcionario ???
+      user = await getRepository(User).save({type,primary_email, secondary_email, password,
+             legal_name, legal_id,
+             address, area, city, state, CEP, mobile_phone});
+      return response.json({message: "user created in database", user});
       break;
     case user_types.professor:
-      const {} = request.body;
+      // TODO validar CPF
+      user = await getRepository(User).save({type,primary_email, secondary_email, password,
+             legal_name, legal_id,
+             address, area, city, state, CEP, mobile_phone});
+      return response.json({message: "user created in database", user});
       break;  
     default:
-      // ERRO
+      return response.json({ message: "error" });
       break;
   }
 };
@@ -52,3 +71,51 @@ export const deleteUser = async (request: Request, response: Response) => {
     return response.json({ message: "error" });
   }
 }
+
+export const updateUser = async (request: Request, response: Response) => {
+  let {type} = request.body;
+  type = Number(type);
+  const {id, primary_email, secondary_email,
+         legal_name, legal_id,
+         address, area, city, state, CEP, mobile_phone} = request.body;
+  // TODO validar email com package isemail
+  // TODO validar email secundario com package isemail
+  // TODO validar password de acordo com as regras que estabelecermos
+  // TODO validar nome????
+  // TODO validar estado
+  // TODO validar CEP
+  // TODO validar celular
+  let update: any;
+  switch (type) {
+    case user_types.candidate:
+      const {registration_number} = request.body;
+      update = await getRepository(User).update(id, {
+        primary_email, secondary_email, legal_name, legal_id,
+         address, area, city, state, CEP, mobile_phone, registration_number
+      });
+      break;
+    case user_types.company:
+      const {alternative_name, employee_name } = request.body;
+      update = await getRepository(User).update(id, {
+        primary_email, secondary_email, legal_name, legal_id,
+         address, area, city, state, CEP, mobile_phone, 
+         alternative_name, employee_name
+      });
+      break;
+    case user_types.professor:
+      update = await getRepository(User).update(id, {
+        primary_email, secondary_email, legal_name, legal_id,
+         address, area, city, state, CEP, mobile_phone
+      });
+      break;
+    default:
+      return response.json({ message: "error" });
+  }
+
+  const user = await getRepository(User).findOne(id);
+  if (update.affected) {
+    return response.json({ message: "user atualizado", user });
+  } else {
+    return response.json({ message: "erro" });
+  }
+};
