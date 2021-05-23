4ed5995c70e6feee64598ae87b352204e60840f5
Luiz Cavalcanti <luizdantas.cavalcanti@gmail.com>
2021-05-23 19:25:57 -0300

    move validation functions to helpers directory

--- API/src/controller/UserController.ts
+++ API/src/controller/UserController.ts
@@ -4,32 +4,14 @@ import { User, user_types } from "../entity/User";
 import { Request, Response } from "express";
 import { genSalt, hash } from "bcrypt";
 import { validate as isValid } from "gerador-validador-cpf";
-
-export const ESTADOS = ["AC","AL","AP", "AM","BA","CE","DF","ES","GO","MA","MT",
-                        "MS","MG","PA","PB","PR","PE","PI","RJ","RN","RS","RO",
-                        "RR","SC","SP","SE","TO",""];
+import {validateVacancyInfo} from "../helpers/validations"
 
 export const getUsers = async (request: Request, response: Response) => {
   const users = await getRepository(User).find();
   return response.json(users);
 };
 
-const validateGenericUser = (primary_email, password,
-                     legal_name, CEP, mobile_phone,state) => {
-  let validations = 0;
-  if (!validate(primary_email, { minDomainAtoms: 2 }))
-    throw new Error(`Email inválido.`);
-  if (password.length < 8)
-    throw new Error(`Senha inválida, necessita ter mais de 8 caracteres.`);
-  if (legal_name.length < 2)
-    throw new Error(`Nome inválido: muito curto.`);
-  if ((CEP != "" && CEP.length != 8) || Number.isNaN(Number(CEP)))
-    throw new Error(`CEP inválido.`);
-  if (!ESTADOS.includes(state))
-    throw new Error(`Estado inválido.`);
-  if (mobile_phone.length != 11 && mobile_phone.length != 0)
-    throw new Error(`Celular inválido.`);
-}
+
 
 
 export const createUser = async (request: Request, response: Response) => {
--- API/src/helpers/validations.ts
+++ API/src/helpers/validations.ts
@@ -23,3 +23,23 @@ export const validateVacancyInfo = (vacancy: VacancyInfo) => {
     throw new Error(`Quem está oferecendo esta vaga?`);
   }
 };
+
+export const ESTADOS = ["AC","AL","AP", "AM","BA","CE","DF","ES","GO","MA","MT",
+                        "MS","MG","PA","PB","PR","PE","PI","RJ","RN","RS","RO",
+                        "RR","SC","SP","SE","TO",""];
+export const validateGenericUser = (primary_email, password,
+                     legal_name, CEP, mobile_phone,state) => {
+  let validations = 0;
+  if (!validate(primary_email, { minDomainAtoms: 2 }))
+    throw new Error(`Email inválido.`);
+  if (password.length < 8)
+    throw new Error(`Senha inválida, necessita ter mais de 8 caracteres.`);
+  if (legal_name.length < 2)
+    throw new Error(`Nome inválido: muito curto.`);
+  if ((CEP != "" && CEP.length != 8) || Number.isNaN(Number(CEP)))
+    throw new Error(`CEP inválido.`);
+  if (!ESTADOS.includes(state))
+    throw new Error(`Estado inválido.`);
+  if (mobile_phone.length != 11 && mobile_phone.length != 0)
+    throw new Error(`Celular inválido.`);
+}
