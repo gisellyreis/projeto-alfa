10ebf63de31819729f4dbbc1f6309fe36a491b50
gisellyreis <gisellyareis@gmail.com>
2021-05-30 19:08:23 -0300

    adiciona algumas rotas no back

--- API/src/routes.ts
+++ API/src/routes.ts
@@ -1,18 +1,19 @@
 import { Router, Request, Response } from "express";
-import {
-  getTasks,
-  createTask,
-  deleteTask,
-  updateTask,
-} from "./controller/TasksController";
+import { createUser, getUsers } from "./controller/UserController";
+
+import { createVacancy, getVacancies } from "./controller/VacanciesController";
 
 const routes = Router();
 
-routes.get("/tasks", getTasks);
-routes.delete("/tasks", deleteTask);
+routes.get("/users", getUsers);
+routes.post("/users", createUser);
+
+routes.get("/vagas", getVacancies);
+routes.post("/vagas", createVacancy);
+/* routes.delete("/tasks", deleteTask);
 routes.post("/tasks", createTask);
 routes.put("/tasks", updateTask);
-
+ */
 routes.get("/", (request: Request, response: Response) => {
   response.json({ message: "ola mundo!" });
 });
