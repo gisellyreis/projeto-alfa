f22a546d79e09b606d5522a6804494d82e4ddd67
ElieneMontenegro <lila.amaral@hotmail.com>
2021-06-02 23:22:50 -0300

    merge

--- API/src/routes.ts
+++ API/src/routes.ts
@@ -1,5 +1,4 @@
 import { Router, Request, Response } from "express";
-<<<<<<< HEAD
 import {
   getUsers,
   createUser,
@@ -24,23 +23,6 @@ routes.get("/vacancy", getVacancies);
 routes.delete("/vacancy", deleteVacancy);
 routes.post("/vacancy", createVacancy);
 routes.put("/vacancy", updateVacancy);
-=======
-
-import { createUser, deleteUser, getUsers, updateUser } from "./controller/UserController";
-import { createVacancy, deleteVacancy, getVacancies, updateVacancy } from "./controller/VacanciesController";
-
-const routes = Router();
-
-routes.get("/users", getUsers);
-routes.post("/users", createUser);
-routes.put("/users", updateUser);
-routes.delete("/users", deleteUser);
-
-routes.get("/vagas", getVacancies);
-routes.post("/vagas", createVacancy);
-routes.put("/vagas", updateVacancy);
-routes.delete("/vagas", deleteVacancy);
->>>>>>> 1904256f96095953dd88ba73aa1db288f36a648b
 
 routes.get("/", (request: Request, response: Response) => {
   response.json({ message: "ola mundo!" });
